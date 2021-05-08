6e16edaa067871ef9d0ee6123c5697e3401475ea
Hardcodex1 <iamrf40@gmail.com>
2021-05-08 15:18:56 +0530

    first commit

--- index.js
+++ index.js
@@ -10,10 +10,45 @@ bot.on('ready' ,() =>
 {
     console.log("BOT ONLINE, PROCEED TO DISCORD");
 
+    bot.user.setPresence({
+        activity: {
+          name: "%help |BOT UNDER TESTING",
+          type: 0,
+        }
+    })
+
     command(bot, "ping", message => 
     {
         message.channel.send("pong");
     })
+
+    command(bot, "server", message =>
+    {
+        bot.guilds.cache.forEach((guild) => {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`);
+        })
+    })
+
+    command(bot, ["cc", "clear"], message => 
+    {
+        if (message.member.hasPermission('Gae')) {
+            message.channel.messages.fetch().then((results) => {
+                message.channel.bulkDelete(results);
+            })
+        }
+    })
+
+    command(bot, 'status', (message) => {
+        const content = message.content.replace('%status ', '')
+        // "!status hello world" -> "hello world"
+    
+        bot.user.setPresence({
+          activity: {
+            name: content,
+            type: 0,
+          },
+        })
+      })
 })
 
 
